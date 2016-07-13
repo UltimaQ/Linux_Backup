1b332b2c62c691eb7d1abd26ff79c9d560c73792
George Wilborn <wingate670@gmail.com>
2016-07-13 00:57:15 -0500

    Add files via upload

---
+++ .switchC.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo rm /dev/dri/card0 && sudo mv /dev/dri/card1 /dev/dri/card0
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+su
+apt-get update
+apt-get install irssi mutt fetchmail procmail git vim leafpad i3blocks arandr lxappearance mc
+exit
