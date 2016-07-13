5e56fa1d6745fa1d19063615df4d261b3d32c3a5
UltimaQ <qbark009@gmail.com>
2016-07-13 01:30:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 su
 apt-get update
-apt-get install irssi mutt fetchmail procmail git vim leafpad i3blocks arandr lxappearance mc
+apt-get install irssi mutt fetchmail procmail vim leafpad i3blocks arandr lxappearance mc
 exit
